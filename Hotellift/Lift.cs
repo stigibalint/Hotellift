927d10b90958851caae4cbf7e5a7485d321a515c
stigibalint <72031994+stigibalint@users.noreply.github.com>
2023-11-15 10:40:06 +0100

    Add files via upload

---
+++ Hotellift/Lift.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotellift
+{
+    public class Lift
+    {
+        string idopont;
+        int kartyasorszam, induloszint, celszint;
+
+        public Lift(string idopont, int kartyasorszam, int induloszint, int celszint)
+        {
+            this.idopont = idopont;
+            this.kartyasorszam = kartyasorszam;
+            this.induloszint = induloszint;
+            this.celszint = celszint;
+        }
+
+        public string Idopont { get => idopont;  }
+        public int Kartyasorszam { get => kartyasorszam;  }
+        public int Induloszint { get => induloszint;  }
+        public int Celszint { get => celszint; }
+
+
+        public Lift(string CSVSOR)
+        {
+            var mezo = CSVSOR.Split(" ");
+            this.idopont = mezo[0];
+            this.kartyasorszam = int.Parse(mezo[1]);
+            this.induloszint = int.Parse(mezo[2]);
+            this.celszint = int.Parse(mezo[3]);
+        }
+    }
+
+}
---
+++ Hotellift/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Hotellift;
+
+List<Lift> adatok = new List<Lift>();
+try
+{
+    File.ReadAllLines("lift.txt").ToList().ForEach(x => adatok.Add(new Lift(x)));
+}
+catch (IOException e)
+{
+    Console.WriteLine($"Hiba történt a fájl olvasása közben: {e.Message}");
+}
+
+Console.WriteLine($"3. feladat: Összes lifthasználat: {adatok.Count}");
+
+Console.WriteLine($"4. feladat: {adatok.MaxBy(x => x.Idopont).Idopont} - {adatok.MinBy(x => x.Idopont).Idopont}");
+
+Console.WriteLine($"5. feladat: Célszint max: {adatok.Max(x => x.Celszint)}");
+
+
+int kartyaBekeres = 0;
+int celBekeres = 0;
+try
+{
+    kartyaBekeres = Convert.ToInt32(Console.ReadLine());
+    celBekeres = Convert.ToInt32(Console.ReadLine());
+}
+catch (Exception)
+{
+    kartyaBekeres = 5;
+    celBekeres = 5; 
+
+}
+Console.WriteLine( $"6. feladat: \n\t Kártya száma: {kartyaBekeres} \n\t Célszint száma: {celBekeres}" );
+
+    Console.WriteLine(adatok.Any(x => x.Kartyasorszam == kartyaBekeres && x.Celszint == celBekeres)
+        ? $"7. feladat: A/Az {kartyaBekeres}. kártyával utaztak a/az {celBekeres} emeletre"
+        : "7. feladat: Nem utaztak.");
+
+File.WriteAllLines("statisztika.txt", adatok.GroupBy(x => x.Idopont).Select(x => $"\t{x.Key} - {x.Count()}x"));
